11e18f8d7cd3c7a931166209300d268740953f7c
Udeme06 <ekpoudemeusen@gmail.com>
2015-12-18 10:07:20 -0500

    Add photo upload capability Using file upload package

--- client/kiosk/kiosk.js
+++ client/kiosk/kiosk.js
@@ -1,20 +1,46 @@
 //helper
+
+
 Template.kiosk.helpers({
 	'updateItems':function(){
-		return items.find();
+		return items.find({});
 	},
-	'otherHelper':function(){
-		//do something to help
-	}
+	'selectedClass':function(){
+		return this._id;
+	},
+	/*uploads:function(){
+		return Uploads.find();
+	}*/
+
+	images: function () {
+    return Images.find(); // Where Images is an FS.Collection instance
+  }
+
 });
 
 Template.kiosk.events({
-	'click #items':function(){
-		//items.insert(price : 350, item: "x-box one", userid: "mark");
-		
-		var itemId = this._id;
-    Session.set('selectedItem', itemId);
-    
-	}
+	'submit form': function(event, template){
+		event.preventDefault();
+		let itemName = template.find('#item-name').value;
+		let price = template.find('#price').value;
+		items.insert({'ItemName': itemName, 'price':price});
+	},
+
+	/*'change .fileInput': function(event, template){
+		FS.Utility.eachFile(event, function(file){
+			var fileObj = new FS.File(file);
+			Uploads.insert(fileObj, function(err){
+				console.log(err);
+			})
+		})
+	}*/
+	'change .fileInput': function(event, template) {
+        FS.Utility.eachFile(event, function(file) {
+        Images.insert(file, function (err, fileObj) {
+        // Inserted new doc with ID fileObj._id, and kicked off the data upload using HTTP
+
+      });
+    });
+  }
 });
 
