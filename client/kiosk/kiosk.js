45b2ead77b343aafd315b3cb3ee50b45c0337c09
Udeme06 <ekpoudemeusen@gmail.com>
2015-12-20 01:33:22 -0500

    modify image upload code

--- client/kiosk/kiosk.js
+++ client/kiosk/kiosk.js
@@ -14,7 +14,7 @@ Template.kiosk.helpers({
 
 	images: function () {
     return Images.find(); // Where Images is an FS.Collection instance
-  }
+}
 
 });
 
@@ -26,21 +26,15 @@ Template.kiosk.events({
 		items.insert({'ItemName': itemName, 'price':price});
 	},
 
-	/*'change .fileInput': function(event, template){
-		FS.Utility.eachFile(event, function(file){
-			var fileObj = new FS.File(file);
-			Uploads.insert(fileObj, function(err){
-				console.log(err);
-			})
-		})
-	}*/
-	'change .fileInput': function(event, template) {
-        FS.Utility.eachFile(event, function(file) {
-        Images.insert(file, function (err, fileObj) {
-        // Inserted new doc with ID fileObj._id, and kicked off the data upload using HTTP
+	
 
-      });
-    });
-  }
+	'change .fileInput': function(event, template) {
+		FS.Utility.eachFile(event, function(file) {
+			Images.insert(file, function (err) {
+				
+			});
+		});
+	}
+
 });
 
