708aa0de417208ff49f7e256e768d62862594aa0
Udeme06 <ekpoudemeusen@gmail.com>
2016-01-13 16:12:27 -0500

    Implement cart for shopping

--- client/buy/buy.js
+++ client/buy/buy.js
@@ -2,25 +2,33 @@ var cart = [];
 
 Template.buy.events({
 	'change #category':function(){
-       var category = $("#category option:selected").val(); 
+		var category = $("#category option:selected").val(); 
        //alert(category);
-      Session.set('category', category);
-	},
+       Session.set('category', category);
+   },
 
-	'click .buy': function(){
-		var item = this._id;
-       cart.join(item);
-	}
+   'click .buy': function(){
+   	var item = this._id;
+
+   	if(cart.indexOf(item)<0){
+   		cart.push(item);
+   		Session.set('increment', cart.length);
+   	}
+   	else{
+   		Session.set('increment', cart.length);
+   	}
+   }
 
 });
 
 Template.buy.helpers({
-  'items': function(){
-  	var categoryName = Session.get('category');
-  	return Items.find({'metadata.category': categoryName});
-  },
+	'items': function(){
+		var categoryName = Session.get('category');
+		return Items.find({'metadata.category': categoryName});
+	},
 
-  'ItemNumber':function(){
-    return cart.length;
-  }
+	'ItemNumber':function(){
+		var no = Session.get('increment');
+		return no;
+	}
 });
