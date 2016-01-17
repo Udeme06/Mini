26e85f5b2dd92a278606a74832d3d1a07f988f8e
Udeme06 <ekpoudemeusen@gmail.com>
2016-01-17 13:22:29 -0500

    Add abstraction so user cannot buy items he advertised in shop

--- client/buy/buy.js
+++ client/buy/buy.js
@@ -1,3 +1,4 @@
+
 var cart = [];
 
 Template.buy.events({
@@ -13,6 +14,11 @@ Template.buy.events({
    	if(cart.indexOf(item)<0){
    		cart.push(item);
    		Session.set('increment', cart.length);
+      
+     /*Session.set('selectedItem',item);*/
+      /*var selectedItem = Session.get('selectedItem');
+      console.log(selectedItem);*/
+    
    	}
    	else{
    		Session.set('increment', cart.length);
@@ -24,11 +30,23 @@ Template.buy.events({
 Template.buy.helpers({
 	'items': function(){
 		var categoryName = Session.get('category');
-		return Items.find({'metadata.category': categoryName});
+		return Items.find({ $and: [ {'metadata.owner':{$ne: Meteor.userId()}}, {'metadata.category': categoryName} ] });
 	},
 
 	'ItemNumber':function(){
 		var no = Session.get('increment');
 		return no;
-	}
+	},
+
+/*'selectedClass': function(){
+  //return "selected"
+  //return this._id
+   var itemId = this._id;
+    var selectedItem = Session.get('selectedItem');
+    if(itemId == selectedItem){
+    return "selected";
+  }*/
+
+//}
+
 });
